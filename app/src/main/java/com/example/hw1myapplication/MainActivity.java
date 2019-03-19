18ec2afa7cafed147e4c4cc38fc6da201df8a758
Vicky <vicky19990109@gmail.com>
2019-03-19 23:58:50 +0800

    HW

---
+++ app/src/main/java/com/example/hw1myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.hw1myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    int size =30;
+    public  void bigger(View v){
+        TextView hello;
+        hello=findViewById(R.id.textView);
+        hello.setTextSize(++size);
+    }
+
+
+
+}
